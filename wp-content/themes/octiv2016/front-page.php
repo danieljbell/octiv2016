26a8e72231be60217f1deecfc59275cdb0e05ac9
danieljbell <djbell70@gmail.com>
2017-02-03 16:16:05 -0500

    changes

--- wp-content/themes/octiv2016/front-page.php
+++ wp-content/themes/octiv2016/front-page.php
@@ -16,7 +16,7 @@ $context = stream_context_create(array(
 	<div class="site-width pos-rel" style="z-index: 2;">
 	    <div class="half white-text">
 	        <div>
-	            <h1><span style="display: block;">Octiv Powers Documents</span> <span style="color: #ed4c06; font-weight: normal; font-style: italic;">for</span> <span class="typed">Global Leaders</span></h1>
+	            <h1><span style="display: block;">Otiv Powers Documents</span> <span style="color: #ed4c06; font-weight: normal; font-style: italic;">for</span> <span class="typed">Global Leaders</span></h1>
 	            <p class="fancy-links">Streamline and automate <a href="#workflows">document workflows</a> that connect <a href="#connections">systems and data</a> for a better <a href="#user-experience">end-user experience</a>. Octiv powers <a href="#documents">documents</a> for sales, legal, finance &amp; human resources - freeing your teams to be <a href="#productive">more productive</a>.</p>
 	        </div>
 	    </div>
