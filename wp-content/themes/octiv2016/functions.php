b162a8e8285b46ee6a1207e83aee1ba082ed6fc7
danieljbell <djbell70@gmail.com>
2017-02-06 17:18:51 -0500

    changing hover for use cases

--- wp-content/themes/octiv2016/functions.php
+++ wp-content/themes/octiv2016/functions.php
@@ -5,7 +5,7 @@ ADD GLOBAL CSS TO PAGE
 ==============================
 */
 function enqueue_global_css() {
-	wp_enqueue_style('style', get_stylesheet_directory_URI() . '/dist/style.css', array(), '2.0.1');
+	wp_enqueue_style('style', get_stylesheet_directory_URI() . '/dist/style.css', array(), '2.0');
 }
 
 add_action('wp_enqueue_scripts', 'enqueue_global_css');
