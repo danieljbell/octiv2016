93e3a8d47bad6eda022c3bb283d3a2af86cef35c
danieljbell <djbell70@gmail.com>
2017-02-07 12:56:24 -0500

    update spacing

--- wp-content/themes/octiv2016/sidebar-landing-form.php
+++ wp-content/themes/octiv2016/sidebar-landing-form.php
@@ -5,7 +5,7 @@
 		if ($post->ID === 1787) :
 			echo '<a href="https://cc.readytalk.com/r/4adm49bfrohw&eom" class="btn-primary">Register Here</a>';
 		elseif ($post->ID === 2279) :
-			echo '<a href="#" class="btn-primary">Read Now</a>';
+			echo '<div class="centered"><a href="#" class="btn-primary">Read Now</a></div>';
 		else : ?>
 		<script src="//app-sj20.marketo.com/js/forms2/js/forms2.min.js"></script>
 		<form id="mktoForm_1041"></form>
