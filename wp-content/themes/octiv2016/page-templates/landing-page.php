6b00fe3475c1ea4de16170e5a31d4e5930d0f8a8
danieljbell <djbell70@gmail.com>
2017-02-08 10:05:38 -0500

    disable compression

--- wp-content/themes/octiv2016/page-templates/landing-page.php
+++ wp-content/themes/octiv2016/page-templates/landing-page.php
@@ -9,6 +9,7 @@ get_header();
 
 ?>
 
+<!-- <div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>);"> -->
 <div class="fixed-hero-section">
 	<div class="site-width white-text">
 		<h1><?php the_title(); ?></h1>
@@ -21,8 +22,6 @@ get_header();
 	<div class="site-width">
 		<div class="two-third">
 			<div>
-				<?php the_post_thumbnail(); ?>
-				<br>
 				<h2>About the <?php the_title(); ?></h2>
 				<hr>
 				<?php the_content(); ?>
