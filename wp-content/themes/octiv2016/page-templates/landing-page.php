994b6c013668d9335725ae7b77bafed56f839c6f
danieljbell <djbell70@gmail.com>
2017-02-08 10:32:12 -0500

    updated landing page display

--- wp-content/themes/octiv2016/page-templates/landing-page.php
+++ wp-content/themes/octiv2016/page-templates/landing-page.php
@@ -9,25 +9,21 @@ get_header();
 
 ?>
 
-<!-- <div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>);">
-	<div class="site-width centered white-text two-third-only"> -->
-<div class="fixed-hero-section">
-	<div class="site-width white-text">
+<div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>);">
+	<div class="site-width centered white-text two-third-only">
 		<div>
 			<h1><?php the_title(); ?></h1>
-			<!-- <p><?php the_excerpt(); ?></p> -->
+			<p><?php the_excerpt(); ?></p>
 		</div>
 	</div>
 </div>
 
-<?php  get_template_part('partials/display', 'breadcrumbs'); ?>
+<?php // get_template_part('partials/display', 'breadcrumbs'); ?>
 
 <section class="page-content">
 	<div class="site-width">
 		<div class="two-third">
 			<div class="content-container">
-				<?php the_post_thumbnail(); ?>
-				<br>
 				<h2>About the <?php the_title(); ?></h2>
 				<hr>
 				<?php the_content(); ?>
