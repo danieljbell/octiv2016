42155d75039289e89bce9161251aada47172f87c
danieljbell <djbell70@gmail.com>
2017-02-09 09:30:36 -0500

    remove svg filter from platform page

--- wp-content/themes/octiv2016/page-templates/platform-page.php
+++ wp-content/themes/octiv2016/page-templates/platform-page.php
@@ -180,8 +180,8 @@ main .half {
 .solutions-container svg {
 	width: 100%;
 	fill: #fff;
-	-webkit-filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.65));
-					filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.65));
+	/*-webkit-filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.65));
+					filter: drop-shadow(2px 2px 5px rgba(0,0,0,0.65));*/
 	max-width: 75px;
 	max-height: 75px;
 }
