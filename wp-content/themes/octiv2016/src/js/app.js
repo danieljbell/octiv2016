9c7d2c4d35c9bd4885577c3f9942a2f685bc8853
danieljbell <djbell70@gmail.com>
2017-02-27 13:23:37 -0500

    marketo cookies should be ready

--- wp-content/themes/octiv2016/src/js/app.js
+++ wp-content/themes/octiv2016/src/js/app.js
@@ -632,7 +632,13 @@ if (window.MktoForms2) {
 
     // Send page URL to Marketo
     $('input[name="sourceURL"]').attr('value', pageURL);
-    $('input[name="LeadSource"]').attr('value', 'Web');
+    var cook = getCookie('ref');
+
+    if (cook) {
+      $('input[name="LeadSource"]').attr('value', cook);
+    } else {
+      $('input[name="LeadSource"]').attr('value', 'Web');
+    }
 
     function removeStyles(formObj) {
       $('#mktoForms2BaseStyle').remove();
@@ -646,6 +652,7 @@ if (window.MktoForms2) {
       formObj.find('.mktoButtonWrap').attr('style', '').parent('.mktoButtonRow').addClass('centered');
       formObj.find('.mktoInset').attr('style', '');
     }
+
   });
 }
 
@@ -696,26 +703,59 @@ if ($('.list-is-collpased')) {
 MARKETO COOKIES
 ==============================
 */
-(function() {
+setCookie();
 
-  var param = getParameterByName('ref');
 
-  if (param) {
-    console.log('True');
-  } else {
-    console.log('False');
-  }
 
-  function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }
-})();
+
+
 
 // end document.ready
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function setCookie() {
+  var param = getParameterByName('ref');
+  var now = new Date();
+  now.setTime(now.getTime()+(30*24*60*60*1000));
+  var expires = "; expires=" + now.toGMTString() + ";";
+
+  if (param) {
+    document.cookie = "ref=" + param + expires + "path=/";
+  }
+
+}
+
+
+function getCookie(name) {
+  var dc = document.cookie;
+  var prefix = name + "=";
+  var begin = dc.indexOf("; " + prefix);
+  if (begin == -1) {
+      begin = dc.indexOf(prefix);
+      if (begin != 0) return null;
+  }
+  else
+  {
+      begin += 2;
+      var end = document.cookie.indexOf(";", begin);
+      if (end == -1) {
+      end = dc.length;
+      }
+  }
+  // because unescape has been deprecated, replaced with decodeURI
+  //return unescape(dc.substring(begin + prefix.length, end));
+  return decodeURI(dc.substring(begin + prefix.length, end));
+}
