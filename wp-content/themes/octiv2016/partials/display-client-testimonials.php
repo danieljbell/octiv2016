b962a8d0f69d1994c8ad31390166330105da0493
danieljbell <djbell70@gmail.com>
2017-03-06 13:59:44 -0500

    let GE pull from the client story

--- wp-content/themes/octiv2016/partials/display-client-testimonials.php
+++ wp-content/themes/octiv2016/partials/display-client-testimonials.php
@@ -14,7 +14,6 @@
 
 		if ( $query->have_posts() ) :
 			echo '<section class=" slider-nav-section" style="background-color: #f0f0f0; padding: 1rem 0;"><div class="slider-nav">';
-			echo '<div><img src="/wp-content/uploads/2017/02/GE.png" alt=""></div>';
 				while ( $query->have_posts() ) :
 					$query->the_post();
 						echo
@@ -23,16 +22,6 @@
 			echo '</div></section>';
 		$query->rewind_posts();
 		echo '<div class="slider-for" role="list">';
-		echo '<div role="listitem" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(/wp-content/uploads/2017/02/ge-slider-bg.jpg);">
-			<div class="site-width">
-				<div class="centered">
-					<img src="/wp-content/uploads/2017/02/GE.png" alt="">
-				</div>
-				<div>
-					<h4>Before Octiv, GE Global Operations had an inefficient contract creation workflow. With Octiv, they\'ve saved a year of man hours in just six months.</h4>
-				</div>
-			</div>
-		</div>';
 		while ( $query->have_posts() ) :
 			$query->the_post();
 				echo
