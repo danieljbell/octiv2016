9bc48fb351502a6513ce674ff0aa84509ba1f7e6
danieljbell <djbell70@gmail.com>
2017-05-09 13:36:24 -0400

    updating feature meta

--- wp-content/themes/octiv2016/single-features.php
+++ wp-content/themes/octiv2016/single-features.php
@@ -99,7 +99,7 @@
     <div class="site-width">
       <div class="centered">
         <h2><?php echo get_field('datasheet_headline'); ?></h2>
-        <p><?php echo get_field('datasheet_headline'); ?></p>
+        <p><?php echo get_field('datasheet_subheadline'); ?></p>
         <button class="datasheet-modal-button btn-primary">Get the Full Datasheet</button>
       </div>
     </div>
