c1ad54ce22a23e81b90cd786176f2100cd8415a6
danieljbell <djbell70@gmail.com>
2017-06-22 17:11:02 -0400

    adding optional cover image to landing pages

--- wp-content/themes/octiv2016/page-templates/landing-page.php
+++ wp-content/themes/octiv2016/page-templates/landing-page.php
@@ -30,6 +30,9 @@ get_header();
 	<div class="site-width">
 		<div class="two-third">
 			<div class="content-container">
+				<?php if (get_field('show_cover_on_lp')) {
+					echo '<img src="' . get_field('cover_image') . '" alt="' . get_the_title() . ' Cover" class="content-cover">';
+				} ?>
 				<?php the_content(); ?>
 			</div>
 			<aside>
