8a432837fe444e768622c07c2663bfd3a801c682
danieljbell <djbell70@gmail.com>
2017-06-26 12:46:29 -0400

    no FOW from listing

--- wp-content/themes/octiv2016/page.php
+++ wp-content/themes/octiv2016/page.php
@@ -31,7 +31,8 @@
 						$args = array(
 							'post_type'		=> array('page','post'),
 							// 'posts_per_page' => -1,
-							'post_parent' => $post->ID
+							'post_parent' => $post->ID,
+							'post__not_in' => array( 3025 )
 						);
 					endif;
 				?>
