490ac858fed0092fe5937cc1e88b8f6c3e5a006c
danieljbell <djbell70@gmail.com>
2017-06-28 14:40:23 -0400

    tax events hero

--- wp-content/themes/octiv2016/taxonomy-event_type.php
+++ wp-content/themes/octiv2016/taxonomy-event_type.php
@@ -5,7 +5,7 @@
   $queried_object = get_queried_object();
 ?>
 
-<div class="fixed-hero-section">
+<div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(/wp-content/themes/octiv2016/dist/img/octiv-pattern.svg), url(/wp-content/uploads/2017/06/events-archive-bg.jpg);">
   <div class="site-width white-text">
     <h1><?php echo $tax_title; ?> Events</h1>
   </div>
