a386154a0e4a438dda00cb9f6d441f56c99f9420
danieljbell <djbell70@gmail.com>
2017-06-30 10:36:39 -0400

    single post hero

--- wp-content/themes/octiv2016/single.php
+++ wp-content/themes/octiv2016/single.php
@@ -16,7 +16,7 @@ the_post();
 
 $upload_dir = wp_upload_dir(); ?>
 
-<div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>);">
+<div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0)), url(<?php echo wp_get_attachment_url( get_post_thumbnail_id() ); ?>);">
 	<div class="site-width centered white-text">
 		<h1><?php the_title(); ?></h1>
 	</div>
