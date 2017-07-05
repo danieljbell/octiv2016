8738379675d44a54b0cf1f26d20d8091cbf16cfa
danieljbell <djbell70@gmail.com>
2017-07-05 10:16:45 -0400

    updating client-story single hero

--- wp-content/themes/octiv2016/page.php
+++ wp-content/themes/octiv2016/page.php
@@ -8,6 +8,9 @@
 <?php get_header(); ?>
 
 <div class="fixed-hero-section" <?php if (get_post_thumbnail_id()) {
+	if ($post->post_parent === 74) {
+		echo 'style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(/wp-content/themes/octiv2016/dist/img/octiv-pattern.svg), url(' . get_field('client_testimonial_image') . ');"';
+	}
 	echo 'style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(/wp-content/themes/octiv2016/dist/img/octiv-pattern.svg), url(' . wp_get_attachment_url( get_post_thumbnail_id() ) . ');"';
 } else {
 	echo 'style="background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(/wp-content/themes/octiv2016/dist/img/octiv-pattern.svg), url(/wp-content/uploads/2017/06/generic-' . $rand_num . '.jpg);"';
@@ -15,6 +18,11 @@
 	<div class="centered site-width white-text">
 		<?php if ($post->post_parent === 74) : ?>
 			<h1><?php echo rtrim(get_field('short_description'), '.'); ?></h1>
+			<div class="two-third-only">
+				<div>
+					<p class="font-bump"><?php echo get_field('client_testimonial'); ?></p>
+				</div>
+			</div>
 		<?php else : ?>
 			<h1><?php the_title(); ?></h1>
 		<?php endif; ?>
