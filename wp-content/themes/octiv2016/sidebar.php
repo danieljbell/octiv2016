99c7ef760f8f226192e34d50f9eb0f611519bf07
danieljbell <djbell70@gmail.com>
2017-07-18 09:24:42 -0400

    temp inline style change of sidebar ad container

--- wp-content/themes/octiv2016/sidebar.php
+++ wp-content/themes/octiv2016/sidebar.php
@@ -12,7 +12,7 @@
 	// ';
 ?>
 
-<div class="ad-container">
+<div class="ad-container" style="background-color: #42b0d8; background-image: linear-gradient(rgba(255,255,255,0.1), rgba(0,0,0,0.1));">
 	<?php
 		$args = array(
 			'post_type' => 'page',
