da1af13a6a7c9a84274eb0026e4dfb9e2c3d1fbb
danieljbell <djbell70@gmail.com>
2017-07-28 14:22:20 -0400

    fixing redirect for webinar

--- wp-content/themes/octiv2016/sidebar-landing-form.php
+++ wp-content/themes/octiv2016/sidebar-landing-form.php
@@ -114,7 +114,7 @@
               followUpUrl = 'http://see.octiv.com/proposals/create_and_view?api_key=35d333a314d6a4a2ceec0a321c111408465d293c&proposal[template_id]=40064&proposal[name]=Experience%20Octiv%20in%20Action&first_name=' + vals.FirstName + '&last_name=' + vals.LastName + '&company_name=' + vals.Company + '&has_integrations=' + hasIntegrations + '&is_salesforce=' + isSalesforce + '&is_dynamics=' + isDynamics + '&is_docusign=' + isDocusign + '&is_steelbrick=' + isSteelbrick + '&is_netsuite=' + isNetsuite + '&is_pipedrive=' + isPipedrive + '&is_oraclecpq=' + isOracleCPQ + '&is_zuoraquotes=' + isZuoraQuotes + '&use_cases=' + vals.poweredUseCase + '&has_features=' + hasFeatures + '&feature_interest=' + vals.octivFeatureInterest;
 						}
 
-						if (window.location.pathname === '/experience/') {
+						if (window.location.pathname === '/platform-demo/') {
               followUpUrl = location.href + '/view/?reg=true&first_name=' + vals.FirstName;
             }
 
