172e3c328a61ddc3a68748444b5931ee5dd84a6a
danieljbell <djbell70@gmail.com>
2017-08-01 13:27:05 -0400

    fixing hero for career singles

--- wp-content/themes/octiv2016/page-templates/careers.php
+++ wp-content/themes/octiv2016/page-templates/careers.php
@@ -10,7 +10,7 @@ get_header();
 
 <?php if (!isset($_GET['gh_jid'])) : ?>
 
-	<div class="fixed-hero-section" style="background-image: linear-gradient(rgba(0,0,0,0.65), rgba(0,0,0,0.65)), url(/wp-content/themes/octiv2016/dist/img/octiv-pattern.svg), url('/wp-content/themes/octiv2016/dist/img/indy-skyline.jpg');">
+	<div class="fixed-hero-section">
 		<div class="site-width centered white-text">
 			<div class="two-third-only">
 				<div>
