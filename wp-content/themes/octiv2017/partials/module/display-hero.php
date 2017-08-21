86b84ca95602f42af1ea0baec02ac94f78067a28
danieljbell <djbell70@gmail.com>
2017-08-21 15:55:57 -0400

    updating client story archive hero

--- wp-content/themes/octiv2017/partials/module/display-hero.php
+++ wp-content/themes/octiv2017/partials/module/display-hero.php
@@ -4,10 +4,18 @@
 HERO
 ==============================
 */
+
+$rand_num = mt_rand(1,4);
 ?>
 
 <?php
-  if ($post->post_type === 'client-stories') {
+  if (is_archive('client-stories')) {
+    $hero_bg = '/wp-content/uploads/2017/06/generic-' . $rand_num . '.jpg';
+    $page_title = 'Client Stories';
+    $page_sub_title = null;
+  }
+
+  if (is_singular('client-stories')) {
     $hero_bg = get_field('client_testimonial_image');
     $page_title = get_field('short_description');
     $page_sub_title = strip_tags( get_the_excerpt() );
@@ -17,6 +25,10 @@ HERO
 <section class="hero" style="background-image: radial-gradient(rgba(45, 57, 67, 0.75),rgba(45, 57, 67, 0)), linear-gradient(rgba(45, 57, 67, 0.7), rgba(45, 57, 67, 0.7)), url(<?php echo $hero_bg; ?>);">
   <div class="site-width">
     <h1><?php echo $page_title; ?></h1>
-    <h2><?php echo $page_sub_title; ?></h2>
+    <?php
+      if ($page_sub_title) {
+        echo '<h2>' . $page_sub_title . '</h2>';
+      }
+    ?>
   </div>
 </section>
--- wp-content/themes/octiv2017/single-client-stories.php
+++ wp-content/themes/octiv2017/single-client-stories.php
@@ -37,26 +37,38 @@
     <?php echo get_field('the_problem'); ?>
   </div>
   <div class="site-width">
-    <h3>The Solution</h3>
+    <h3 class="pad-t">The Solution</h3>
     <?php echo get_field('the_solution'); ?>
   </div>
-  <div class="client-story-quote">
-    <div class="site-width">
-      <blockquote>
-        <div class="quote"><?php echo get_field('highlighted_quote'); ?></div>
-        <div class="person-details-container">
-          <div class="person-headshot">
-            <img src="<?php echo get_field('person_headshot'); ?>" alt="">
+  <?php
+    if (get_field('highlighted_quote')) :
+  ?>
+    <div class="client-story-quote">
+      <div class="site-width">
+        <blockquote>
+          <div class="quote"><?php echo get_field('highlighted_quote'); ?></div>
+            <?php
+              if (get_field('person_headshot')) :
+                echo '<div class="person-details-container has-person-headshot">';
+                  echo '<div class="person-headshot">';
+                    echo '<img src="' . get_field('person_headshot') . '" alt="">';
+                  echo '</div>';
+              else :
+                echo '<div class="person-details-container">';
+              endif;                
+            ?>
+            <div class="person-details-content">
+              <p class="person-name"><?php echo get_field('person_name'); ?></p>
+              <p class="person-title"><?php echo get_field('person_title'); ?></p>
+              <p class="person-company"><?php echo get_field('person_company'); ?></p>
+            </div>
           </div>
-          <div class="person-details-content">
-            <p class="person-name"><?php echo get_field('person_name'); ?></p>
-            <p class="person-title"><?php echo get_field('person_title'); ?></p>
-            <p class="person-company"><?php echo get_field('person_company'); ?></p>
-          </div>
-        </div>
-      </blockquote>
+        </blockquote>
+      </div>
     </div>
-  </div>
+  <?php
+    endif;
+  ?>
   <div class="site-width">
     <?php
       if (get_field('application_screenshot')) {
@@ -67,7 +79,13 @@
         echo '</div>';
       }
     ?>
-    <h3 class="pad-t">The Result</h3>
+    <?php
+      if (get_field('highlighted_quote')) :
+        echo '<h3>The Result</h3>';
+      else :
+        echo '<h3 class="pad-t">The Result</h3>';
+      endif;
+    ?>
     <?php echo get_field('the_results'); ?>
   </div>
 </section>
