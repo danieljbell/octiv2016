166f300dcdc8a3445b9b647478fc2effc84ae132
danieljbell <djbell70@gmail.com>
2017-08-21 11:59:23 -0400

    adding context to file get contents

--- wp-content/themes/octiv2017/footer.php
+++ wp-content/themes/octiv2017/footer.php
@@ -1,3 +1,13 @@
+<?php
+$user = 'octiv';
+$pass = 'D@n13lR0cks!';
+$context = stream_context_create(array(
+  'http' => array(
+    'header'  => "Authorization: Basic " . base64_encode("$user:$pass")
+  )
+));
+?>
+
 <div class="modal-container empty-modal">
   <button class="close-modal">Close <span class="close-icon">+</span></button>
   <div class="modal-content">
@@ -91,13 +101,13 @@
 <footer class="site-footer">
   <div class="site-width footer-utility-links">
     <ul class="social-links">
-      <li><a href="https://www.linkedin.com/company-beta/10866770/" title="Octiv's LinkedIn Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/linkedin-icon.svg'); ?></a></li>
-      <li><a href="http://twitter.com/octivinc" title="Octiv's Twitter Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/twitter-icon.svg'); ?></a></li>
-      <li><a href="https://www.instagram.com/octivinc/" title="Octiv's Instagram Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/instagram-icon.svg'); ?></a></li>
-      <li><a href="http://facebook.com/OctivInc" title="Octiv's Facebook Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/facebook-icon.svg'); ?></a></li>
+      <li><a href="https://www.linkedin.com/company-beta/10866770/" title="Octiv's LinkedIn Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/linkedin-icon.svg', false, $context); ?></a></li>
+      <li><a href="http://twitter.com/octivinc" title="Octiv's Twitter Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/twitter-icon.svg', false, $context); ?></a></li>
+      <li><a href="https://www.instagram.com/octivinc/" title="Octiv's Instagram Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/instagram-icon.svg', false, $context); ?></a></li>
+      <li><a href="http://facebook.com/OctivInc" title="Octiv's Facebook Page"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/facebook-icon.svg', false, $context); ?></a></li>
     </ul>
     <div class="site-footer-logo">
-      <a href="/" title="Home"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/Octiv-Logo.svg'); ?></a>
+      <a href="/" title="Home"><?php echo file_get_contents(get_stylesheet_directory_URI() . '/dist/img/Octiv-Logo.svg', false, $context); ?></a>
     </div>
     <div class="newsletter-subscription-container">
       <button id="newsletter-subscription" class="btn-primary">Get Our Newsletter</button>
