f1cec3348adec6ad1bae0f13d882a1e47edd2b70
danieljbell <djbell70@gmail.com>
2017-09-29 14:31:55 -0400

    busting cache

--- wp-content/themes/octiv2017/functions.php
+++ wp-content/themes/octiv2017/functions.php
@@ -5,7 +5,7 @@ ADD GLOBAL CSS TO PAGE
 ==============================
 */
 function enqueue_global_css() {
-  wp_enqueue_style('global', get_stylesheet_directory_URI() . '/dist/css/global.css', array(), '1.0.6');
+  wp_enqueue_style('global', get_stylesheet_directory_URI() . '/dist/css/global.css', array(), '1.0.7');
 }
 add_action('wp_enqueue_scripts', 'enqueue_global_css');
 
@@ -15,7 +15,7 @@ ADD GLOBAL JS TO PAGE
 ==============================
 */
 function enqueue_global_js() {
-  wp_enqueue_script('global', get_stylesheet_directory_URI() . '/dist/js/global.js', array(), '1.0.2', true);
+  wp_enqueue_script('global', get_stylesheet_directory_URI() . '/dist/js/global.js', array(), '1.0.3', true);
   if (is_post_type_archive('integration') || is_singular('integration')) {
     wp_enqueue_script('integrations', get_stylesheet_directory_URI() . '/dist/js/integrations.js', array(), '1.0.0', true);
   }
