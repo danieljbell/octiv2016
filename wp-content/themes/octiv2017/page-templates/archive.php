c66433e9f93b51c3e6b4374d76b999660aaccff7
danieljbell <djbell70@gmail.com>
2017-10-16 12:27:53 -0400

    debug

--- wp-content/themes/octiv2017/page-templates/archive.php
+++ wp-content/themes/octiv2017/page-templates/archive.php
@@ -43,6 +43,7 @@ Template Name: Archive
                 <a v-bind:href="post.link">
                   <!-- <img v-bind:src="post.acf.integration_logo" v-bind:alt="post.title.rendered"> -->
                   <h4>{{post.title.rendered}}</h4>
+                  <pre>{{post}}</pre>
                 </a>
               </div>
             <?php else : ?>
