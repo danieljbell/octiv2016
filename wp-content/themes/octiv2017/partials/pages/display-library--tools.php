72f4e6c8035ab9841e3cfea2bc1d77689d9865c6
danieljbell <djbell70@gmail.com>
2017-10-19 16:40:14 -0400

    default redirect is /view/?reg=true

--- wp-content/themes/octiv2017/partials/pages/display-library--tools.php
+++ wp-content/themes/octiv2017/partials/pages/display-library--tools.php
@@ -1,3 +1,11 @@
+<?php
+  $form_id = get_field('marketo_form_id');
+  $redirect_link = $_SERVER['REQUEST_URI'] . 'view/?reg=true';
+  if (get_field('form_redirect_link')) {
+    $redirect_link = get_field('form_redirect_link');
+  }
+?>
+
 <?php get_template_part('partials/module/display', 'hero'); ?>
 
 <section class="notch">
@@ -16,9 +24,9 @@
   <div class="site-width">
     <div class="half-only">
       <div class="color-boxes" style="margin-bottom: 0.5rem;">
-        <h2 class="color-box-headline--white">Get the Whitepaper Now</h2>
+        <h2 class="color-box-headline--white">Get the <?php echo get_the_title(); ?> Now</h2>
       </div>
-      <p>Fill out the form below and you'll get access to the whitepaper.</p>
+      <p>Fill out the form below and you'll get access to the <?php echo get_the_title(); ?>.</p>
       <div class="two-third-only">
         <script src="//app-sj20.marketo.com/js/forms2/js/forms2.min.js"></script>
         <form id="mktoForm_<?php echo $form_id; ?>"></form>
