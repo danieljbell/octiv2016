b685a8f2ccb01e6ad278fd9d2d3336808e7d1ab6
danieljbell <djbell70@gmail.com>
2017-10-20 13:05:55 -0400

    fixing bug for blog vue instance

--- wp-content/themes/octiv2017/src/js/pages/resources.js
+++ wp-content/themes/octiv2017/src/js/pages/resources.js
@@ -2,6 +2,9 @@ $.ajax({
   dataType: "json",
   async: false,
   url: "/wp-json/wp/v2/" + pagePostType + "?_embed&order=" + postOrder + "&per_page=" + postsPerPage,
+  error: function() {
+    console.log('oops!');
+  },
   success: function(data) {
     var app = new Vue({
       el: '#searchable-resources',
@@ -34,10 +37,12 @@ $.ajax({
           return this.postList.filter((post) => {
             if (window.location.href.indexOf("client-stories") > -1) {
               return post.title.rendered.toLowerCase().includes(this.keyword.toLowerCase());
-            } else {
+            } else if (window.location.href.indexOf("library") > -1) {
               if (post.parent <= 0) {
                 return (post.title.rendered.toLowerCase().includes(this.keyword.toLowerCase())) && (post._embedded['wp:term'][0][0].slug.includes(this.selectedCats));
               }
+            } else {
+              return (post.title.rendered.toLowerCase().includes(this.keyword.toLowerCase())) && (post._embedded['wp:term'][0][0].slug.includes(this.selectedCats));
             }
           });
         }
