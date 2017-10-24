095babf89ed2198c203f0e54da33fc949860f443
danieljbell <djbell70@gmail.com>
2017-10-24 09:27:55 -0400

    greenhouse good to go

--- wp-content/themes/octiv2017/page-templates/careers.php
+++ wp-content/themes/octiv2017/page-templates/careers.php
@@ -4,52 +4,66 @@
 Template Name: Careers
 ==============================
 */
-
+$greenhouse_page = parse_str($_SERVER['gh_jid']);
 ?>
 
 <?php get_header(); ?>
 
-<main>
+  <main>
 
-  <?php get_template_part('partials/module/display', 'hero'); ?>
+    <?php get_template_part('partials/module/display', 'hero'); ?>
 
-  <?php get_template_part('partials/module/display', 'breadcrumbs'); ?>
+    <?php get_template_part('partials/module/display', 'breadcrumbs'); ?>
 
-  <section>
-    <div class="site-width">
-      <div class="half pad-y-more">
-        <div>
-          <h2>Come Join Our Growing Team</h2>
-          <p>Octiv was founded to provide sales teams a better way to create the assets they need to close deals. We’re passionate about bringing insight, analytics and automation to sales and have earned our position as an industry leader by empowering hundreds of companies around the world to create, deliver and track sales materials online.</p>
-          <a href="/company/careers/#openings" class="btn-primary">View All Opportunities</a>
+    <?php if (isset($_GET['gh_jid'])) : ?>
+      
+      <section id="openings">
+        <div class="site-width">
+          <div id="grnhse_app"></div>
         </div>
-        <div style="background-image: url(/wp-content/uploads/2016/07/team-togetherness.jpg); background-size: cover; background-position: center;">
-          <!-- <img src="/wp-content/uploads/2016/07/team-togetherness.jpg" alt="Team" style="width: 100%;"> -->
+      </section>
+
+    <?php else : ?>
+
+      <section class="pad-y-most">
+        <div class="site-width">
+          <div class="half">
+            <div>
+              <h2>Come Join Our Growing Team</h2>
+              <p>Octiv was founded to provide sales teams a better way to create the assets they need to close deals. We’re passionate about bringing insight, analytics and automation to sales and have earned our position as an industry leader by empowering hundreds of companies around the world to create, deliver and track sales materials online.</p>
+              <a href="/company/careers/#openings" class="btn-primary">View All Opportunities</a>
+            </div>
+            <div style="background-image: url(/wp-content/uploads/2016/07/team-togetherness.jpg); background-size: cover; background-position: center;">
+              <!-- <img src="/wp-content/uploads/2016/07/team-togetherness.jpg" alt="Team" style="width: 100%;"> -->
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
+      </section>
 
-  <section id="employee-testimonials">
-    <ul class="third employee-testimonials-list">
-      <?php get_template_part('partials/module/query', 'employee-testimonials'); ?>
-    </ul>
-  </section>
+      <section id="employee-testimonials" class="pad-y-most">
+        <ul class="third employee-testimonials-list">
+          <?php get_template_part('partials/module/query', 'employee-testimonials'); ?>
+        </ul>
+      </section>
 
-  <section id="openings">
-    <div class="site-width">
-      <h2 class="has-text-center">Current Openings</h2>
-      <div id="grnhse_app"></div>
-    </div>
-  </section>
+      <section id="openings">
+        <div class="site-width">
+          <h2 class="has-text-center">Current Openings</h2>
+          <div id="grnhse_app"></div>
+        </div>
+      </section>
 
-  <?php
-    $greenhouse_page = parse_str($_SERVER['gh_jid']);
-    if ($post->ID === 148 || $greenhouse_page) :
-      echo '<script src="https://boards.greenhouse.io/embed/job_board/js?for=octiv"></script>';
-    endif;
-  ?>
-  
-</main>
+    <?php endif; ?>
+
+    <?php
+      if ($post->ID === 148 || $greenhouse_page) :
+        echo '<script src="https://boards.greenhouse.io/embed/job_board/js?for=octiv"></script>';
+      endif;
+    ?>
+    
+  </main>
+<?php //endif; ?>
+
+
 
 <?php get_footer(); ?>
