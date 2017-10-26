4bdc736f5392f69b045cd527d60dd205020e2151
danieljbell <djbell70@gmail.com>
2017-10-26 14:14:31 -0400

    jumping back to reg_nav way of things

--- wp-content/themes/octiv2017/partials/module/display-site-header.php
+++ wp-content/themes/octiv2017/partials/module/display-site-header.php
@@ -12,21 +12,34 @@ SITE HEADER
         <a href="/" title="Home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/Octiv-Logo.svg" alt="Octiv Logo" class="site-header-logo"></a>
       </li>
       <li class="site-header-top-item site-header-nav-container">
-        <nav>
-          <ul class="top-level-navigation">
+        <?php
+          wp_nav_menu(
+            array(
+              'menu' => 'site-header',
+              'container' => 'nav',
+              'items_wrap' => '<ul id="%1$s" class="%2$s top-level-navigation">%3$s</ul>',
+            )
+          );
+
+        ?>
+        <!-- <nav>
+          <ul class="top-level-navigation"> -->
             <?php
-              $site_header_items = wp_get_nav_menu_items('site-header');
-              $item_iteration = 0;
-              foreach ($site_header_items as $item) {
-                if ($item->menu_item_parent === '0') {
-                  $item_iteration++; ?>
-                  <li class="top-level-navigation-item item-<?php echo $item_iteration; ?>"><a href="<?php echo $item->url; ?>" title="<?php echo $item->title; ?>"><?php echo $item->title; ?></a></li>
-            <?php
-                }
-              }
+              // $site_header_items = wp_get_nav_menu_items('site-header');
+              // $item_iteration = 0;
+              // $top_level_pages = array();
+              // foreach ($site_header_items as $item) :
+              //   if ($item->menu_item_parent === '0') {
+              //     array_push($top_level_pages, $item->ID);
+              //   }
+              // endforeach;
+              // $all_nav_item_ids = array();
+              // foreach ($site_header_items as $item) :
+              //   array_push($all_nav_item_ids, $item->ID);
+              // endforeach;
             ?>
-          </ul>
-        </nav>
+          <!-- </ul>
+        </nav> -->
       </li>
       <li class="site-header-top-item site-header-rad-container">
         <button id="site-head-rad" class="btn-primary rad-modal" data-modal="rad">Request a Demo</button>
@@ -37,3 +50,8 @@ SITE HEADER
     </ul>
   </div>
 </header>
+
+
+
+
+<!-- <li class="top-level-navigation-item item-<?php echo $item_iteration; ?>"><a href="<?php echo $item->url; ?>" title="<?php echo $item->title; ?>"><?php echo $item->title; ?></a></li> -->
