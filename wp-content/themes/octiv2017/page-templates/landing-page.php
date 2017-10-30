2f01881071dba8fad5bab3cd1c3429c9853519d0
danieljbell <djbell70@gmail.com>
2017-10-30 17:00:40 -0400

    init landing page page-template

--- wp-content/themes/octiv2017/page-templates/landing-page.php
+++ wp-content/themes/octiv2017/page-templates/landing-page.php
@@ -13,22 +13,20 @@ get_header();
 
   <?php get_template_part('partials/module/display', 'hero'); ?>
 
-  <section class="page-content">
+  <section class="notch" style="margin-bottom: -3rem;">
     <div class="site-width">
-      <div class="two-third">
-        <div class="content-container">
-          <?php if (get_field('show_cover_on_lp')) {
-            echo '<img src="' . get_field('cover_image') . '" alt="' . get_the_title() . ' Cover" class="content-cover">';
-          } ?>
-          <?php the_content(); ?>
-        </div>
-        <aside>
-          asdfasf
-        </aside>
+      <div class="box--light">
+        <?php the_content(); ?>
       </div>
     </div>
   </section>
-  
+
+  <section class="brand-two-callout pad-y-most" style="padding-top: 6rem;">
+    <div class="site-width">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi veniam, excepturi a, sequi vel quasi voluptatum aperiam nulla eligendi nesciunt.
+    </div>
+  </section>
+
 </main>
 
 <?php get_footer(); ?>
