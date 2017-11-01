c632863b0f2ce09337b70ddce305bdea133e688d
danieljbell <djbell70@gmail.com>
2017-11-01 15:54:22 -0400

    updating buyers journey

--- wp-content/themes/octiv2016/dist/js/buyer-journey/main.js
+++ wp-content/themes/octiv2016/dist/js/buyer-journey/main.js
@@ -42,17 +42,11 @@ $(window).scroll(function() {
 
 });
 
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
+
+$('#toc a').on('click', function(e) {
+  e.preventDefault();
+  var target = $(this.hash);
+  $('html, body').animate({
+      scrollTop: target.offset().top
+  }, 300);
 });
