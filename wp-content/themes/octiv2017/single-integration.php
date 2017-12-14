ab7dd14a4b61ba3ebf4efacd9dcf4d9e668b910c
danieljbell <djbell70@gmail.com>
2017-12-14 17:05:32 -0500

    something with conditionals probs

--- wp-content/themes/octiv2017/single-integration.php
+++ wp-content/themes/octiv2017/single-integration.php
@@ -1,5 +1,11 @@
 <?php get_header(); ?>
 
+<?php
+  if (get_field('integration_video_id')) {
+    $has_video = true;
+  }
+?>
+
 <?php $customer_success = get_field('client_stories'); ?>
 
 <style>
@@ -19,63 +25,19 @@
   
   <?php get_template_part('partials/module/display', 'hero'); ?>
 
-  <?php get_template_part('partials/module/display', 'breadcrumbs'); ?>
+  <?php
+    if (!$has_video) {
+      get_template_part('partials/module/display', 'breadcrumbs');
+    }
+  ?>
 
   <section class="post-content">
     <div class="site-width">
-      <div class="one-fourth">
-        <aside id="sidebar">
-          <div class="sidebar__inner">
-            <h4><?php echo get_the_title(); ?></h4>
-            <hr>
-            <ul>
-              <li><a href="#why">Why Octiv &amp; <?php echo get_the_title(); ?></a></li>
-              <li><a href="#key-capabilities">Key Capabilities</a></li>
-              <li><a href="#technical-requirements">Technical Requirements</a></li>
-              <?php if ($customer_success) : ?>
-                <li><a href="#customer-success">Success Stories</a></li>
-              <?php endif; ?>
-              <li><a href="#about">About <?php echo get_the_title(); ?></a></li>
-            </ul>
-          </div>
-        </aside>
+      <div class="two-third-only">
         <article>
           <h3 id="why">Why Octiv &amp; <?php echo get_the_title(); ?></h3>
           <?php echo get_the_content(); ?>
-          <?php if (get_field('integration_video_id')) : ?>
-              <div class="mar-y-more box--light integration-video-container" style="padding: 2rem; border: 1px solid #ccc; border-radius: 3px; box-shadow: 2px 2px 30px rgba(0,0,0,0.1);">
-                <?php if ($_GET['demo_auth']) : ?>
-                <div class="video-outer">
-                  <div class="video-inner">
-                    <iframe src="https://www.youtube.com/embed/<?php echo get_field('integration_video_id'); ?>?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;VQ=HD720" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" width="100%" height="100%" style="box-shadow: 0 0 15px rgba(0,0,0,0.15);"></iframe>
-                  </div>
-                </div>
-                <?php else : ?>
-                  <div class="half">
-                    <div class="">
-                      <img src="<?php echo get_field('integration_video_thumbnail'); ?>" alt="" style="width: 100%;" class="video-thumbnail">
-                    </div>
-                    <div>
-                      <h4>See Octiv + <?php echo get_the_title(); ?> in Action</h4>
-                      <p>Fill out the form below to view a brief demonstration.</p>
-                      <script src="//app-sj20.marketo.com/js/forms2/js/forms2.min.js"></script>
-                      <form id="mktoForm_<?php echo get_field('integration_video_marketo_form_id'); ?>"></form>
-                      <script>
-                        MktoForms2.loadForm("//app-sj20.marketo.com", "625-MXY-689", <?php echo get_field('integration_video_marketo_form_id'); ?>, function(form) {
-                          form.onSuccess(function(values, followUpUrl) {
-                            form.getFormElem().hide();
-                            var videoContainer = document.querySelector('.integration-video-container');
-                            var videoHTML = document.querySelector('#video-html');
-                            videoContainer.innerHTML = videoHTML.innerHTML;
-                            return false;
-                          });
-                        });
-                      </script>
-                    </div>
-                  </div>
-                <?php endif; ?>
-              </div>
-            <?php endif; ?>
+          
           <h3 id="key-capabilities" class="pad-t">Key Capabilities</h3>
           <?php
             if( have_rows('key_capabilities') ):
@@ -156,3 +118,49 @@
 <?php endif; ?>
 
 <?php get_footer(); ?>
+
+
+
+
+
+<?php
+            /*
+            ==============================
+            <?php if (get_field('integration_video_id')) : ?>
+              <div class="mar-y-more box--light integration-video-container" style="padding: 2rem; border: 1px solid #ccc; border-radius: 3px; box-shadow: 2px 2px 30px rgba(0,0,0,0.1);">
+                <?php if ($_GET['demo_auth']) : ?>
+                <div class="video-outer">
+                  <div class="video-inner">
+                    <iframe src="https://www.youtube.com/embed/<?php echo get_field('integration_video_id'); ?>?rel=0&amp;showinfo=0&amp;modestbranding=1&amp;VQ=HD720" frameborder="0" allowfullscreen="allowfullscreen" mozallowfullscreen="mozallowfullscreen" msallowfullscreen="msallowfullscreen" oallowfullscreen="oallowfullscreen" webkitallowfullscreen="webkitallowfullscreen" width="100%" height="100%" style="box-shadow: 0 0 15px rgba(0,0,0,0.15);"></iframe>
+                  </div>
+                </div>
+                <?php else : ?>
+                  <div class="half">
+                    <div class="">
+                      <img src="<?php echo get_field('integration_video_thumbnail'); ?>" alt="" style="width: 100%;" class="video-thumbnail">
+                    </div>
+                    <div>
+                      <h4>See Octiv + <?php echo get_the_title(); ?> in Action</h4>
+                      <p>Fill out the form below to view a brief demonstration.</p>
+                      <script src="//app-sj20.marketo.com/js/forms2/js/forms2.min.js"></script>
+                      <form id="mktoForm_<?php echo get_field('integration_video_marketo_form_id'); ?>"></form>
+                      <script>
+                        MktoForms2.loadForm("//app-sj20.marketo.com", "625-MXY-689", <?php echo get_field('integration_video_marketo_form_id'); ?>, function(form) {
+                          form.onSuccess(function(values, followUpUrl) {
+                            form.getFormElem().hide();
+                            var videoContainer = document.querySelector('.integration-video-container');
+                            var videoHTML = document.querySelector('#video-html');
+                            videoContainer.innerHTML = videoHTML.innerHTML;
+                            return false;
+                          });
+                        });
+                      </script>
+                    </div>
+                  </div>
+                <?php endif; ?>
+              </div>
+            <?php endif; ?>
+            ==============================
+            */
+            
+          ?>
