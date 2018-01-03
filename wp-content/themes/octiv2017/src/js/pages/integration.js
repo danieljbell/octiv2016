9debae540621e516c32b0ec1132304116e09c950
danieljbell <djbell70@gmail.com>
2018-01-03 09:39:34 -0500

    removing sidebar links on integrations and busting client js

--- wp-content/themes/octiv2017/src/js/pages/integration.js
+++ wp-content/themes/octiv2017/src/js/pages/integration.js
@@ -1,25 +1,6 @@
 (function() {
-/*
-==================================
-SMOOTH SCROLLING FOR SIDEBAR LINKS
-==================================
-*/
-$('aside a').on('click', function(e) {
-  e.preventDefault();
-  var target = $(this.hash);
-  $('html, body').animate({
-      scrollTop: target.offset().top - (document.querySelector('.site-header').offsetHeight)
-  }, 300);
-});
 
 var initialPath = window.location.pathname;
 window.history.replaceState( {} , 'bar', initialPath );
 
 })();
-
-var sidebar = new StickySidebar('#sidebar', {
-  containerSelector: '.post-content',
-  innerWrapperSelector: '.sidebar__inner',
-  topSpacing: (document.querySelector('.site-header').offsetHeight + 18),
-  bottomSpacing: -54
-});
