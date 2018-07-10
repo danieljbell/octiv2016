e3bf34945c0a95ae29eaf98ced7d15e46ea31eea
Daniel Bell <dbell@Daniels-MacBook-Pro-2.local>
2018-07-09 22:37:38 -0400

    https for pardot

--- wp-content/themes/octiv2017/partials/pages/display-library--tools.php
+++ wp-content/themes/octiv2017/partials/pages/display-library--tools.php
@@ -40,7 +40,7 @@
           <iframe src="<?php echo $redirect_link; ?>" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
         <?php endif; ?>
         <?php if ($form_id) : ?>
-          <iframe src="http://go.getconga.com/l/217282/2018-04-30/3dmm3" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
+          <iframe src="https://go.pardot.com/l/217282/2018-04-30/3dmm3" width="100%" height="500" type="text/html" frameborder="0" allowTransparency="true" style="border: 0"></iframe>
         <?php endif; ?>
       </div>
     </div>
